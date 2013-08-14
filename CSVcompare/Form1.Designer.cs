5b54c0d97c6c78b37fc848900f3bad63bac090fd
David Christensen <bondslaveofJesus@gmail.com>
2013-08-14 02:00:54 -0400

    first commit, beta version

---
+++ CSVcompare/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CSVcompare
+{
+    partial class FrmCSVcmpr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCSVcmpr));
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblFile1 = new System.Windows.Forms.Label();
+            this.lblFile2 = new System.Windows.Forms.Label();
+            this.txtbxFile1 = new System.Windows.Forms.TextBox();
+            this.txtbxFile2 = new System.Windows.Forms.TextBox();
+            this.chkbxExcel = new System.Windows.Forms.CheckBox();
+            this.OpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.btnCompare.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.btnCompare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompare.Location = new System.Drawing.Point(8, 84);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(106, 53);
+            this.btnCompare.TabIndex = 0;
+            this.btnCompare.Text = "&Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.btnQuit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuit.Location = new System.Drawing.Point(304, 84);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(106, 53);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "&Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // lblFile1
+            // 
+            this.lblFile1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblFile1.Location = new System.Drawing.Point(60, 3);
+            this.lblFile1.Name = "lblFile1";
+            this.lblFile1.Size = new System.Drawing.Size(87, 36);
+            this.lblFile1.TabIndex = 2;
+            this.lblFile1.Text = "Sunday";
+            this.lblFile1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFile2
+            // 
+            this.lblFile2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblFile2.Location = new System.Drawing.Point(63, 39);
+            this.lblFile2.Name = "lblFile2";
+            this.lblFile2.Size = new System.Drawing.Size(84, 36);
+            this.lblFile2.TabIndex = 3;
+            this.lblFile2.Text = "Wednesday";
+            this.lblFile2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtbxFile1
+            // 
+            this.txtbxFile1.BackColor = System.Drawing.Color.White;
+            this.txtbxFile1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtbxFile1.Location = new System.Drawing.Point(153, 13);
+            this.txtbxFile1.Name = "txtbxFile1";
+            this.txtbxFile1.ReadOnly = true;
+            this.txtbxFile1.Size = new System.Drawing.Size(258, 20);
+            this.txtbxFile1.TabIndex = 4;
+            this.txtbxFile1.Text = "<Click here to browse for file>";
+            this.txtbxFile1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtbxFile1.Click += new System.EventHandler(this.txtbxFile1_Click);
+            // 
+            // txtbxFile2
+            // 
+            this.txtbxFile2.BackColor = System.Drawing.Color.White;
+            this.txtbxFile2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtbxFile2.Location = new System.Drawing.Point(153, 49);
+            this.txtbxFile2.Name = "txtbxFile2";
+            this.txtbxFile2.ReadOnly = true;
+            this.txtbxFile2.Size = new System.Drawing.Size(258, 20);
+            this.txtbxFile2.TabIndex = 5;
+            this.txtbxFile2.Text = "<Click here to browse for file>";
+            this.txtbxFile2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtbxFile2.Click += new System.EventHandler(this.txtbxFile2_Click);
+            // 
+            // chkbxExcel
+            // 
+            this.chkbxExcel.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkbxExcel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.chkbxExcel.Checked = true;
+            this.chkbxExcel.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkbxExcel.FlatAppearance.BorderSize = 2;
+            this.chkbxExcel.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.chkbxExcel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.chkbxExcel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.chkbxExcel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkbxExcel.Font = new System.Drawing.Font("Arial Unicode MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkbxExcel.Location = new System.Drawing.Point(156, 84);
+            this.chkbxExcel.Name = "chkbxExcel";
+            this.chkbxExcel.Size = new System.Drawing.Size(106, 53);
+            this.chkbxExcel.TabIndex = 6;
+            this.chkbxExcel.Text = "Open Excel  -  ✓";
+            this.chkbxExcel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chkbxExcel.UseVisualStyleBackColor = false;
+            this.chkbxExcel.CheckedChanged += new System.EventHandler(this.chkbxExcel_CheckedChanged);
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.AddExtension = false;
+            this.OpenFile.DefaultExt = "csv";
+            this.OpenFile.FileName = "YourList.csv";
+            this.OpenFile.Title = "Choose a file to open:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CSVcompare.Properties.Resources.note_accept;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmCSVcmpr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(422, 158);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chkbxExcel);
+            this.Controls.Add(this.txtbxFile2);
+            this.Controls.Add(this.txtbxFile1);
+            this.Controls.Add(this.lblFile2);
+            this.Controls.Add(this.lblFile1);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnCompare);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmCSVcmpr";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "  CSV Compare";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblFile1;
+        private System.Windows.Forms.Label lblFile2;
+        private System.Windows.Forms.TextBox txtbxFile1;
+        private System.Windows.Forms.TextBox txtbxFile2;
+        private System.Windows.Forms.CheckBox chkbxExcel;
+        private System.Windows.Forms.OpenFileDialog OpenFile;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CSVcompare/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CSVcompare
+{
+    public partial class FrmCSVcmpr : Form
+    {
+        public FrmCSVcmpr()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void chkbxExcel_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkbxExcel.Checked)
+            {
+                chkbxExcel.Text = "Open Excel  -  ✓";
+            }
+            else
+            {
+                chkbxExcel.Text = "Open Excel  -  ?";
+            }
+        }
+
+        private void txtbxFile1_Click(object sender, EventArgs e)
+        {            
+            if (Directory.Exists("C:\\Users\\" + Environment.UserName))
+            {
+                OpenFile.InitialDirectory = "C:\\Users\\" + Environment.UserName;
+            }
+            else if (Directory.Exists("C:\\Documents and Settings\\" + Environment.UserName))
+            {
+                OpenFile.InitialDirectory = "C:\\Documents and Settings\\" + Environment.UserName;
+            }
+
+            DialogResult result; 
+
+            //OpenFile.FileName = BaseFileName;
+            result = OpenFile.ShowDialog();
+            if (result == DialogResult.Cancel)
+            {
+                //the user decided against starting
+                //DispMsg("You must select a file!");
+            }
+            else
+            {
+                txtbxFile1.Text = OpenFile.FileName;
+            }
+
+        }
+
+        private void txtbxFile2_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists("C:\\Users\\" + Environment.UserName))
+            {
+                OpenFile.InitialDirectory = "C:\\Users\\" + Environment.UserName;
+            }
+            else if (Directory.Exists("C:\\Documents and Settings\\" + Environment.UserName))
+            {
+                OpenFile.InitialDirectory = "C:\\Documents and Settings\\" + Environment.UserName;
+            }
+
+            DialogResult result;
+
+            //OpenFile.FileName = BaseFileName;
+            result = OpenFile.ShowDialog();
+            if (result == DialogResult.Cancel)
+            {
+                //the user decided against starting
+                //DispMsg("You must select a file!");
+            }
+            else
+            {
+                txtbxFile2.Text = OpenFile.FileName;
+            }
+        }
+
+        private void DispMsg(string msg2disp)
+        {
+            MessageBox.Show(msg2disp, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            if (txtbxFile1.Text == "<Click here to browse for file>" || txtbxFile2.Text == "<Click here to browse for file>")
+            {
+                DispMsg("You must select a file for File # 1 & 2!!");
+            }
+            else if (txtbxFile2.Text == txtbxFile1.Text)
+            {
+                DispMsg("You've selected the same file for each!!!");
+                txtbxFile2.Text = "<Click here to browse for file>";
+            }
+            else
+            {
+                //do stuff
+                CompareFiles(txtbxFile1.Text, txtbxFile2.Text);
+            }
+        }
+
+        private void CompareFiles(string f1, string f2)
+        {
+            //string f1_contents = File.ReadAllText(f1);
+            string[] f1_contents = File.ReadAllLines(f1);
+            //string f2_contents = File.ReadAllText(f2);
+            string[] f2_contents = File.ReadAllLines(f2);
+            string tmpstring1 = "";
+            string tmpstring2 = "";
+            bool WasFoundInFile = false;
+            string CurrentFileName = OpenFile.InitialDirectory + "\\" + "FileCompare" + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".csv"; //unique for every run
+            int i = 0;
+            int k = 0;
+
+            for (i = 0; i < f1_contents.Length; i++)
+            {
+                for (k = 0; k < f2_contents.Length; k++)
+                {
+                    tmpstring1 = f1_contents[i].Remove(f1_contents[i].LastIndexOf(",")); //clear off grade level
+                    tmpstring2 = f2_contents[k].Remove(f2_contents[k].LastIndexOf(",")); // ^^
+                    if (String.Equals(tmpstring1, tmpstring2, StringComparison.OrdinalIgnoreCase)) //case insensitive search
+                    {
+                        //this line is in both files
+                        WasFoundInFile = true;
+                        break; //leave inner for loop, search no further
+                    }
+                    else if (tmpstring2.Contains(tmpstring1.Remove(tmpstring1.IndexOf(","))+"/"))
+                    {
+                        //this line is in both files, there are some shortened names in files noted by slashes
+                        WasFoundInFile = true;
+                        break; //leave inner for loop, search no further
+                    }
+                }
+                if (!WasFoundInFile)
+                {
+                    if (!File.Exists(CurrentFileName))
+                    {
+                        //write column headers for csv file
+                        using (StreamWriter SW = new StreamWriter(CurrentFileName, true))   //true makes it append to the file instead of overwrite
+                        {
+                            SW.WriteLine("File Absent From,First Name,Last Name,Class"); //column headers for csv
+                            SW.Close();
+                        }
+                    }
+                    using (StreamWriter SW = new StreamWriter(CurrentFileName, true))   //true makes it append to the file instead of overwrite
+                    {
+                        SW.WriteLine(lblFile2.Text +","+ f1_contents[i].ToString());
+                        SW.Close();
+                    }
+                }
+                else
+                {
+                    WasFoundInFile = false;
+                }
+            }
+
+            WasFoundInFile = false;
+
+            for (i = 0; i < f2_contents.Length; i++)
+            {
+                for (k = 0; k < f1_contents.Length; k++)
+                {
+                    tmpstring1 = f1_contents[k].Remove(f1_contents[k].LastIndexOf(",")); //clear off grade level
+                    tmpstring2 = f2_contents[i].Remove(f2_contents[i].LastIndexOf(",")); // ^^
+                    if (String.Equals(tmpstring2, tmpstring1, StringComparison.OrdinalIgnoreCase)) //case insensitive
+                    {
+                        //this line is in both files
+                        WasFoundInFile = true;
+                        break; //leave inner for loop, search no further
+                    }
+                    else if (tmpstring1.Contains((tmpstring2.Remove(tmpstring2.IndexOf(",")) + "/")))
+                    {
+                        //this line is in both files, there are some shortened names in files noted by slashes
+                        WasFoundInFile = true;
+                        break; //leave inner for loop, search no further
+                    }
+                }
+                if (!WasFoundInFile)
+                {
+                    if (!File.Exists(CurrentFileName))
+                    {
+                        //write column headers for csv file
+                        using (StreamWriter SW = new StreamWriter(CurrentFileName, true))   //true makes it append to the file instead of overwrite
+                        {
+                            SW.WriteLine("File Absent From,First Name,Last Name,Class"); //column headers for csv
+                            SW.Close();
+                        }
+                    }
+                    using (StreamWriter SW = new StreamWriter(CurrentFileName, true))   //true makes it append to the file instead of overwrite
+                    {
+                        SW.WriteLine(lblFile1.Text +","+ f2_contents[i].ToString());
+                        SW.Close();
+                    }
+                }
+                else
+                {
+                    WasFoundInFile = false;
+                }
+            }
+
+            DispMsg("All Done!");
+
+            if (chkbxExcel.Checked)
+            {
+                //open csv in excel
+                Excel.Application excelApp = new Excel.Application();
+                excelApp.Visible = true;
+                excelApp.Workbooks.OpenText(CurrentFileName);
+            }
+
+
+        }
+    }
+}
